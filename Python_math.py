7780f2e3401dc31c04975a75438aae0b65b1018d
RomanPrograma <126255710+RomanPrograma@users.noreply.github.com>
2023-03-19 23:04:24 +0200

    ex_5 done

--- Python_math.py
+++ Python_math.py
@@ -1,1 +1,8 @@
 
+number = int(input('number'))
+number_1 = int(number / 1000)
+number_2 = int(number / 100) % 10
+number_3 = int(number / 10) % 10
+number_4 = int(number % 10) 
+result = (number_4*1000)+(number_3*100)+(number_2*10) + number_1 
+print(result)
